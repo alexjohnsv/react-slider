af42e155fbe95eeedb101f6f2840f100d8f4d192
alexjohnsv <alexjohnsv@gmail.com>
2020-11-28 16:59:37 +0100

    render another slider on the demo page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Slider defaultValue={10} marks={[10,55,100]} />
+      <div>
+        <Slider defaultValue={20} marks={[20,30]} />
+      </div>
+      <div>
+        <Slider defaultValue={0} marks={[0,10,20,30,40,50,60,70,80,90,100]} />
+      </div>
     </div>
   );
 }
